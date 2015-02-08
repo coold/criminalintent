b8daf75f2971d59fba494834ae1a584314f3c1ec
coold <rdcoold@gmail.com>
2015-02-08 17:59:51 +0500

    added fragment

---
+++ app/src/main/java/ru/coold/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package ru.coold.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by rz on 08.02.2015.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
